875d192a6b131a3bdb3a45d520c04931be82e1ee
Andreas Frey <frey.andi@hotmail.com>
2021-04-13 18:48:48 +0200

    first push

---
+++ Assets/Scripts/FirstPerson/FirstPersonCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonCamera : MonoBehaviour
+{
+    public Transform playerObject;
+
+    [SerializeField]
+    private Vector2 mouseSensitivity = new Vector2(50, 50);
+    
+    float xRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity.x * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity.y * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerObject.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler((xRotation), 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController controller;
+
+    public float speed = 10f;
+    public float jumpHeight = 5f;
+    bool isGrounded;
+
+    //physics settings
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Jump
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded) {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity); 
+        }
+
+        //physics (gravity)
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0) {
+            velocity.y = -2;
+        }
+        //RaycastHit(Vector3.down);
+    }
+}
